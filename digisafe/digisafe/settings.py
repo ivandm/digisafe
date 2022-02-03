e41607e3b45a72974247a05884e6471425a365d6
ivandm <ivandelmastro@gmail.com>
2022-02-03 12:19:36 +0100

    ...

--- digisafe/digisafe/settings.py
+++ digisafe/digisafe/settings.py
@@ -145,6 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
