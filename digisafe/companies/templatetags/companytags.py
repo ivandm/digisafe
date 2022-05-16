20d9cccf2491e642c046cae7501260c537a95351
ivandm <ivandelmastro@gmail.com>
2022-05-16 14:40:53 +0200

    minor fix

--- digisafe/companies/templatetags/companytags.py
+++ digisafe/companies/templatetags/companytags.py
@@ -12,3 +12,9 @@ def pending_request(request, user):
         if user.requestassociatepending_set.filter(company=c):
             return True
     return False
+
+
+@register.filter
+def sort_by(queryset, order):
+    order = [x.strip() for x in order.split(',')]
+    return queryset.order_by(*order)
